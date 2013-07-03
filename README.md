a9f7984912d687a9b2d0e552614ef3cceca20a03
ctstone <ctstone@gmail.com>
2013-07-02 20:20:48 -0400

    meta updates

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-json
-====
+# csjson
+
+## install
+**PM> Install-Package csjson**
+
+## dynamic output
+```csharp
+dynamic result = JsonTokenizer.Parse(input);
+foreach (var item in result.SomeArray)
+{
+  // stuff
+}
+```
+
+## deserialize
+```csharp
+MyType obj = JsonTokenizer.Parse<MyType>(input);
+// includes DateTime parsing
+```
+
+## serialization
+// not included (yet)
--- ctstone.Json/Properties/AssemblyInfo.cs
+++ ctstone.Json/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ctstone.Json")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("csjson")]
+[assembly: AssemblyDescription("JSON parser that generates either a dynamic object or a strongly typed object")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ctstone.Json")]
+[assembly: AssemblyCompany("Chris Stone")]
+[assembly: AssemblyProduct("csjson")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
